eb2c7e03a6e97c365a403e0aea5ca21c1ccec8c6
DmitryBuryanov <buryanov.dv@edu.spbstu.ru>
2020-05-11 22:15:13 +0300

    Версия без багов

--- src/main/java/view/CheckerModel.java
+++ src/main/java/view/CheckerModel.java
@@ -30,7 +30,6 @@ public class CheckerModel extends StackPane {
         else circle.setFill(Color.WHITESMOKE);
         relocate((x + 0.1) * MainApp.size, (y + 0.1) * MainApp.size);
         setOnMouseDragged(e -> {
-            //relocate(e.getSceneX(), e.getSceneY());
             relocate((Math.floor(e.getSceneX() / MainApp.size) + 0.1) * MainApp.size,
                     (Math.floor(e.getSceneY() / MainApp.size) + 0.1) * MainApp.size);
         });
